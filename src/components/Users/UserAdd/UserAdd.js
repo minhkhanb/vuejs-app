229ef420d4b315321f7db2dbf26272399c195a52
Vi Ha <viha@success-ss.com.vn>
2018-04-09 18:07:46 +0700

    fix small

--- src/components/Users/UserAdd/UserAdd.js
+++ src/components/Users/UserAdd/UserAdd.js
@@ -1,6 +1,29 @@
 import Vue from 'vue';
 
 const UserAddComponent = Vue.extend({
+    components: {
+    },
+    props: {
+    },
+    methods: {
+        cancel() {
+            return false;
+        },
+        addUser() {
+            return true;
+        }
+    },
+    beforeUpdate: function(e) {
+
+    },
+    data() {
+        return {
+            userAdd: {
+                selectedDate: '',
+                name: '',
+            }
+        }
+    }
 });
 
 export default UserAddComponent;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 
 import UserAdd from './UserAdd/index';
-import ListUser from './UserList/index';
+import UserList from './UserList/index';
 
 const UsersComponent = Vue.extend({
   components: {
     appUserAdd: UserAdd,
-    appUserList: ListUser,
+    appUserList: UserList,
   },
 });
 
